f6eb8f79beeb22ecec180a8d0672d3f46685d956
Dharshib <Dharshib.8@gmail.com>
2023-12-29 21:32:53 +0530

    add text field composable function

---
+++ app/src/main/java/com/example/bmicalculator/InputTextField.kt
@@ -0,0 +1,32 @@
+package com.example.bmicalculator
+
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputTextField(value: String, onValueChange: () -> Unit, imeAction: ImeAction) {
+    BasicTextField(
+        value = value,
+        onValueChange = {
+            onValueChange()
+        },
+        textStyle = TextStyle(
+            fontSize = 45.sp,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.secondary
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Decimal,
+            imeAction = imeAction
+        ),
+        cursorBrush = SolidColor(MaterialTheme.colorScheme.primary)
+    )
+}
